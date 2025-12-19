db8df5ff72d3d3355e25265bb33e9e8dc6ccf068
Krzysztof Kolodziej <krzysztof.kolodziej2212@gmail.com>
2025-12-19 12:25:28 +0100

    chore: initial Spring Boot project setup

---
+++ src/main/java/com/example/windsurf_weather_service/WindsurfWeatherServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.windsurf_weather_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WindsurfWeatherServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WindsurfWeatherServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/windsurf_weather_service/WindsurfWeatherServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.windsurf_weather_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WindsurfWeatherServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
