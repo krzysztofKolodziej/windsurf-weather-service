bbc7e26c788697aa725311ec2ebf52074b4d974c
Krzysztof Kolodziej <krzysztof.kolodziej2212@gmail.com>
2025-12-19 18:06:47 +0100

    test: add Weatherbit client tests for success and error cases

--- src/main/java/com/example/windsurf_weather_service/client/WeatherbitDailyDataDto.java
+++ src/main/java/com/example/windsurf_weather_service/client/WeatherbitDailyDataDto.java
@@ -1,7 +1,12 @@
 package com.example.windsurf_weather_service.client;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public record WeatherbitDailyDataDto(
+        @JsonProperty("valid_date")
         String validDate,
+        @JsonProperty("temp")
         double temp,
+        @JsonProperty("wind_spd")
         double windSpeed
 ) {}
--- src/main/java/com/example/windsurf_weather_service/client/WeatherbitDailyForecastResponse.java
+++ src/main/java/com/example/windsurf_weather_service/client/WeatherbitDailyForecastResponse.java
@@ -1,9 +1,14 @@
 package com.example.windsurf_weather_service.client;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.List;
 
 public record WeatherbitDailyForecastResponse(
+        @JsonProperty("city_name")
         String cityName,
+        @JsonProperty("country_code")
         String countryCode,
+        @JsonProperty("data")
         List<WeatherbitDailyDataDto> data
 ) { }
